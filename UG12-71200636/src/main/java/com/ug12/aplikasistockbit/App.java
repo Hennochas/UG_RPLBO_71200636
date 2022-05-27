e1623cd8f0411c200b66d1c77c3780d7afd4b6b9
Hennochas <78341298+Hennochas@users.noreply.github.com>
2022-05-27 10:27:45 +0700

    Add files via upload

---
+++ UG12-71200636/src/main/java/com/ug12/aplikasistockbit/App.java
@@ -0,0 +1,13 @@
+package com.ug12.aplikasistockbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UG12-71200636/src/main/java/com/ug12/aplikasistockbit/Saham.java
@@ -0,0 +1,2 @@
+public class Saham {
+}
